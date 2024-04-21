2373a99a4a325c6dd44dc12e4c712522c06376a9
Wrajt <kgrabarek59@gmail.com>
2024-04-21 11:05:23 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestGoogleApiKeyComponent } from './test-google-api-key/test-google-api-key.component';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestGoogleApiKeyComponent],
+  imports: [RouterOutlet, TestGoogleApiKeyComponent, MatToolbar, MatIconModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  playlistID = "PL1F7117E03613D657"
+}

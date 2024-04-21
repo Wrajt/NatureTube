37f17a566b881d2cb7555cd46a8dacee82742c01
Wrajt <kgrabarek59@gmail.com>
2024-04-21 11:08:28 +0200

    Revert "Initial commit"
    This reverts commit 2373a99a4a325c6dd44dc12e4c712522c06376a9.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestGoogleApiKeyComponent } from './test-google-api-key/test-google-api-key.component';
-import {MatToolbar} from "@angular/material/toolbar";
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestGoogleApiKeyComponent, MatToolbar, MatIconModule, MatButtonModule],
+  imports: [RouterOutlet, TestGoogleApiKeyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  playlistID = "PL1F7117E03613D657"
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,3 @@
 import { Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
 
-export const routes: Routes = [
-  { path: 'home',
-  component: HomeComponent,
-    pathMatch: 'full',
-},
-  { path: '',
-  redirectTo: '/home',
-    pathMatch: 'full',
-}
-]
+export const routes: Routes = [];

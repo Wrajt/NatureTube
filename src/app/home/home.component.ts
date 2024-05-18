e4b0f648550a9a16f5985c1ecad5dea574b2fd36
Wrajt <kgrabarek59@gmail.com>
2024-05-18 14:06:24 +0200

    mostly styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,33 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {ApiService} from "../api.service";
-import {SingleVideoInterface} from "../interfaces/single-video.interface";
-import {NgForOf} from "@angular/common";
-import {MatList, MatListItem} from "@angular/material/list";
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { SingleVideoInterface } from '../interfaces/single-video.interface';
+import { NgForOf } from '@angular/common';
+import { MatList, MatListItem } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-    imports: [
-        NgForOf,
-        MatList,
-        MatListItem
-    ],
+  imports: [NgForOf, MatList, MatListItem, MatCardModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit{
-  playlistID = "PL1F7117E03613D657"
-  playlistName = ""
-  playlistItems: SingleVideoInterface[] = []
+export class HomeComponent implements OnInit {
+  playlistID = 'PL1F7117E03613D657';
+  playlistName = '';
+  playlistItems: SingleVideoInterface[] = [];
   ngOnInit(): void {
-   this.apiService.getPlaylistName(this.playlistID).subscribe((playlist) => {
-     this.playlistName = playlist.items[0].snippet.title
-   })
+    this.apiService.getPlaylistName(this.playlistID).subscribe((playlist) => {
+      this.playlistName = playlist.items[0].snippet.title;
+    });
 
-    this.apiService.getPlaylistItems(this.playlistID).subscribe((playlistItems) => {
-      this.playlistItems = playlistItems.items
-      console.log(this.playlistItems)
-    })
+    this.apiService
+      .getPlaylistItems(this.playlistID)
+      .subscribe((playlistItems) => {
+        this.playlistItems = playlistItems.items;
+        console.log(this.playlistItems);
+      });
   }
-constructor(private apiService: ApiService) {
-}}
+  constructor(private apiService: ApiService) {}
+}

887e593e08b3bc9884bd505af84cfbd4e1b4856a
Wrajt <kgrabarek59@gmail.com>
2024-05-18 13:08:48 +0200

    safe pipe, video component finished

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SafePipe } from '../safe.pipe';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-video',
   standalone: true,
-  imports: [],
+  imports: [SafePipe, MatIconModule],
   templateUrl: './video.component.html',
-  styleUrl: './video.component.scss'
+  styleUrl: './video.component.scss',
 })
-export class VideoComponent {
+export class VideoComponent implements OnInit {
+  videoId = '';
+  videoUrl = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((item) => {
+      this.videoId = item['id'];
+      this.videoUrl =
+        'https://www.youtube.com/embed/' +
+        this.videoId +
+        '?si=0MfiEUX3LOHwPi3g';
+    });
+  }
 }

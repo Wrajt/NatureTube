018dc9f254feed50865fc1163d4e1d0e30fbb5fb
Wrajt <kgrabarek59@gmail.com>
2024-05-18 15:38:32 +0200

    more or less working app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { SingleVideoInterface } from '../interfaces/single-video.interface';
-import { NgForOf } from '@angular/common';
+import { CommonModule, NgForOf } from '@angular/common';
 import { MatList, MatListItem } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgForOf, MatList, MatListItem, MatCardModule],
+  imports: [
+    NgForOf,
+    MatList,
+    MatListItem,
+    MatCardModule,
+    MatTabsModule,
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-  playlistID = 'PL1F7117E03613D657';
-  playlistName = '';
-  playlistItems: SingleVideoInterface[] = [];
-  ngOnInit(): void {
-    this.apiService.getPlaylistName(this.playlistID).subscribe((playlist) => {
-      this.playlistName = playlist.items[0].snippet.title;
-    });
+  readonly playlistID = [
+    'PL1F7117E03613D657',
+    'PLpbCMGDjn19gq5GSYdWDoM1vVpqJ44JHd',
+  ];
+  playlistNames: string[] = [];
+  playlistItems: SingleVideoInterface[][] = [];
 
-    this.apiService
-      .getPlaylistItems(this.playlistID)
-      .subscribe((playlistItems) => {
-        this.playlistItems = playlistItems.items;
+  ngOnInit(): void {
+    this.playlistID.forEach((item) => {
+      this.apiService.getPlaylistName(item).subscribe((playlist) => {
+        this.playlistNames.push(playlist.items[0].snippet.title);
+      });
+      this.apiService.getPlaylistItems(item).subscribe((items) => {
+        this.playlistItems.push(items.items);
         console.log(this.playlistItems);
       });
+    });
   }
+
   constructor(private apiService: ApiService) {}
 }
